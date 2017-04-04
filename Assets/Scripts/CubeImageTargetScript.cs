31962f5e291cdaaf2531d9f0bd83b8a4525ec0d6
Visnja Krsmanovic <visnja.krsmanovic@zuhlke.com>
2017-04-04 14:47:55 +0200

    Using gravity
    Signed-off-by: Visnja Krsmanovic <visnja.krsmanovic@zuhlke.com>

---
+++ Assets/Scripts/CubeImageTargetScript.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class CubeImageTargetScript : MonoBehaviour, IInputClickHandler
+{
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        gameObject.AddComponent<Rigidbody>();
+    }
+}
