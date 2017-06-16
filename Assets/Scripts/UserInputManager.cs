286b0727881dd889eb7b58a2191812d65baafd6e
Aleksandar Bojkovic <aleksandar.bojkovic@zuehlke.com>
2017-06-16 09:54:44 +0200

    Added the third button and implemented toggling of text based on the button pressed.

---
+++ Assets/Scripts/UserInputManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInputManager : MonoBehaviour
+{
+    public Text AboutText;
+    public Text LocationsText;
+    public Text TechnologiesText;
+        
+    public void About()
+    {
+        LocationsText.enabled = false;
+        TechnologiesText.enabled = false;
+        AboutText.enabled = true;
+    }
+
+    public void Locations()
+    {
+        TechnologiesText.enabled = false;
+        AboutText.enabled = false;
+        LocationsText.enabled = true;
+    }
+
+    public void Technologies()
+    {
+        AboutText.enabled = false;
+        LocationsText.enabled = false;
+        TechnologiesText.enabled = true;
+    }
+}
